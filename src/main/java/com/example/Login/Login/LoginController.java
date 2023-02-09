9cd804ad69b49a006a5047f8180a768339d8a2ce
markxkamau <markxkamau@gmail.com>
2023-02-09 13:12:25 +0300

    Added the logout function and button in the reference page

--- src/main/java/com/example/Login/Login/LoginController.java
+++ src/main/java/com/example/Login/Login/LoginController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -23,6 +25,31 @@ public class LoginController {
         model.addAttribute("login_info", new LoginDto());
         return "Login_Page";
     }
+    @GetMapping("/logout")
+    public String logout(HttpSession session, HttpServletResponse response) {
+        session.invalidate();
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        return "redirect:/reference";
+    }
+
+    @GetMapping("/reference")
+    public String showReferencePage(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Object userInfo = session.getAttribute("user_info");
+
+        if (userInfo == null) {
+            // If the user is not logged in, redirect them to the login page
+            return "redirect:/login";
+        }
+
+        // Add the user information to the model so it can be displayed on the reference page
+        model.addAttribute("user_info", userInfo);
+
+        return "Reference";
+    }
+
 
     @PostMapping("/login")
     public String startLoginSession(HttpServletRequest httpServletRequest, @ModelAttribute LoginDto loginDto, @NotNull RedirectAttributes redirectAttributes) {
@@ -32,6 +59,6 @@ public class LoginController {
         }
         HttpSession httpSession = httpServletRequest.getSession();
         httpSession.setAttribute("user_info", loginService.getUserInfo(loginDto));
-        return "Reference";
+        return "redirect:/reference";
     }
 }
